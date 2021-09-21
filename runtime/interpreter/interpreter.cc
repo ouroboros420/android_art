da7c62cb6ce2d9e9e8ca7c3d6ec2838f50cf5950
Mythri Alle <mythria@google.com>
2021-09-21 15:09:58 +0000

    Drop unused arguments for MethodEntered function
    this_object and dex_pc aren't used in MethodEntered listener.
    
    Test: art/test/run-test
    Change-Id: I158f962befe1015d21d08c757d4c415fe7865d16
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- runtime/interpreter/interpreter.cc
+++ runtime/interpreter/interpreter.cc
@@ -317,10 +317,7 @@ static inline JValue Execute(
 
     instrumentation::Instrumentation* instrumentation = Runtime::Current()->GetInstrumentation();
     if (UNLIKELY(instrumentation->HasMethodEntryListeners())) {
-      instrumentation->MethodEnterEvent(self,
-                                        shadow_frame.GetThisObject(accessor.InsSize()),
-                                        method,
-                                        0);
+      instrumentation->MethodEnterEvent(self, method);
       if (UNLIKELY(shadow_frame.GetForcePopFrame())) {
         // The caller will retry this invoke or ignore the result. Just return immediately without
         // any value.
