a184fa3e60d2dc8abf33add48019863c94582645
Stefano Cianciulli <scianciulli@google.com>
2022-06-09 14:46:08 +0000

    Fix modernize-use-using clang-tidy issues
    All the // NOLINT comments introduced in this CL were meant
    to disable only the modernize-use-using checks, but changing
    the comment to be // NOLINT(modernize-use-using) as explained
    in https://clang.llvm.org/extra/clang-tidy/#suppressing-undesired-diagnostics
    will make cpplint throw an error, as also cpplint uses the
    // NOLINT(...) notation to suppress unwanted warnings.
    
    Test: m tidy-art
    Test: art/libnativebridge/tests/runtests.sh
    Bug: 213953102
    Google: 2123594
    
    Change-Id: I9a992e0f0ecd06f7e64fd0bbf473bec4582370f6
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- libartbase/base/data_hash.h
+++ libartbase/base/data_hash.h
@@ -44,6 +44,7 @@ class DataHash {
       uint32_t hash = Murmur3Start();
 
       const size_t nblocks = length_in_bytes / 4;
+      using unaligned_uint32_t __attribute__((__aligned__(1))) = uint32_t;
       typedef __attribute__((__aligned__(1))) uint32_t unaligned_uint32_t;
       const unaligned_uint32_t* blocks = reinterpret_cast<const unaligned_uint32_t*>(data);
       for (size_t i = 0; i != nblocks; ++i) {
